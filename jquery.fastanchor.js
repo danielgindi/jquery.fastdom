562a1457839f3d1a444fcc4324d1321f8500220b
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-06-09 13:01:06 +0300

    Play nice with other plugins

--- jquery.fastanchor.js
+++ jquery.fastanchor.js
@@ -7,11 +7,7 @@
     Version: 2014-06-09
 */
 (function($){
-    
-    var didScroll = false, scrollHandler = function () {
-        didScroll = true;
-    };
-    
+        
     function touchById(touches, touchId) {
         for (var i = 0; i < touches.length; i++) {
             if (touches[i].identifier === touchId) return touches[i];
@@ -24,47 +20,48 @@
         if (event.target.tagName !== 'A') return; // Skip this element
         
         var touchId = event.originalEvent.changedTouches[0].identifier,
+            didScroll = false, 
+            scrollHandler = function () {
+                didScroll = true;
+            },
             active = true;
         
-        didScroll = false;
-                    
         var onCancel = function () {
             $this
                 .removeClass('active') // Remove active class
                 .off('.fastanchor'); // Unhook events
                 
-            $(window)
-                .off('scroll.fastanchor', scrollHandler) // Stop tracking scroll;
+            $(window).off('scroll.fastanchor', scrollHandler) // Stop tracking scroll
         };
         
-        $(window)
-            .on('scroll.fastanchor', scrollHandler); // Start tracking scroll to prevent "click" after scroll
+        $(window).on('scroll.fastanchor', scrollHandler); // Start tracking scroll to prevent "click" after scroll
 
-        var $this = $(event.target)
-                    .addClass('active') // Add active class
+        var $this = $(event.target).addClass('active') // Add active class
          
-        $this.on('touchmove.fastanchor', function (event) { // Start tracking touch movement to see if we are still on top
-        
-            var touch = touchById(event.originalEvent.changedTouches, touchId);
-            if (!touch) return;
+        $this
+            .addClass('active') // Add active class
+            .on('touchmove.fastanchor', function (event) { // Start tracking touch movement to see if we are still on top
+            
+                var touch = touchById(event.originalEvent.changedTouches, touchId);
+                if (!touch) return;
 
-            var element = document.elementFromPoint(touch.pageX, touch.pageY);
-            active = element && (this === element || $.contains(this, element));
-            $this.toggleClass('active', active);
+                var element = document.elementFromPoint(touch.pageX, touch.pageY);
+                active = element && (this === element || $.contains(this, element));
+                $this.toggleClass('active', active);
+                
+            }).on('touchend.fastanchor', function (event) {
             
-        }).on('touchend.fastanchor', function (event) {
-        
-            var touch = touchById(event.originalEvent.changedTouches, touchId);
-            if (!touch) return;
-            
-            onCancel();
-            
-            if (active && !didScroll) {
-                this.click(); // Trigger native event
-                event.preventDefault();
-            }
-            
-        }).on('touchcancel.fastanchor', onCancel);
+                var touch = touchById(event.originalEvent.changedTouches, touchId);
+                if (!touch) return;
+                
+                onCancel();
+                
+                if (active && !didScroll && !event.isDefaultPrevented()) {
+                    this.click(); // Trigger native event
+                    event.preventDefault();
+                }
+                
+            }).on('touchcancel.fastanchor', onCancel);
     
     });
 
