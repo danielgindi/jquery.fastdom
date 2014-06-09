8d9b534595ae144ed31eebbaf0f52cf7016dfe05
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-06-09 12:49:01 +0300

    Track leaving the anchors

--- jquery.fastanchor-min.js
+++ jquery.fastanchor-min.js
@@ -1,6 +1,7 @@
 /*
     Library by Daniel Cohen Gindi (danielgindi@gmail.com) 054-5655765 
     MIT License!
-    Version: 2014-02-18
+    Version: 2014-06-09
 */
-(function(a){function b(){c=!0}var c=!1;a(window).on("touchstart.fastanchor",function(e){if("A"===e.target.tagName){c=!1;a(window).on("scroll",b);var d=a(this).addClass("active");d.on("touchend.fastanchor",function(){d.removeClass("active").off("touchend.fastanchor").off("touchcancel.fastanchor");a(window).off("scroll",b);c||this.click()}).on("touchcancel.fastanchor",function(){d.removeClass("active").off("touchend.fastanchor").off("touchcancel.fastanchor");a(window).off("scroll",b)})}})})(jQuery);
+(function(c){function g(){e=!0}function h(d,c){for(var b=0;b<d.length;b++)if(d[b].identifier===c)return d[b];return null}var e=!1;c(window).on("touchstart.fastanchor",function(d){if("A"===d.target.tagName){var k=d.originalEvent.changedTouches[0].identifier,b=!0;e=!1;var l=function(){f.removeClass("active").off(".fastanchor");c(window).off("scroll.fastanchor",g)};c(window).on("scroll.fastanchor",g);var f=c(d.target).addClass("active");f.on("touchmove.fastanchor",function(a){if(a=h(a.originalEvent.changedTouches,
+k))b=(a=document.elementFromPoint(a.pageX,a.pageY))&&(this===a||c.contains(this,a)),f.toggleClass("active",b)}).on("touchend.fastanchor",function(a){h(a.originalEvent.changedTouches,k)&&(l(),b&&!e&&(this.click(),a.preventDefault()))}).on("touchcancel.fastanchor",l)}})})(jQuery);
--- jquery.fastanchor.js
+++ jquery.fastanchor.js
@@ -4,7 +4,7 @@
 // ==/ClosureCompiler==
 /** @preserve    Library by Daniel Cohen Gindi (danielgindi@gmail.com) 054-5655765 
     MIT License!
-    Version: 2014-02-18
+    Version: 2014-06-09
 */
 (function($){
     
@@ -12,33 +12,59 @@
         didScroll = true;
     };
     
+    function touchById(touches, touchId) {
+        for (var i = 0; i < touches.length; i++) {
+            if (touches[i].identifier === touchId) return touches[i];
+        }
+        return null;
+    }
+    
     $(window).on('touchstart.fastanchor', function (event) {
     
         if (event.target.tagName !== 'A') return; // Skip this element
         
+        var touchId = event.originalEvent.changedTouches[0].identifier,
+            active = true;
+        
         didScroll = false;
-        $(window).on('scroll', scrollHandler);
-        
-        var $this = $(this)
-                    .addClass('active') // Add hover class
-        
-        $this.on('touchend.fastanchor', function () {
+                    
+        var onCancel = function () {
             $this
-                .removeClass('active') // Remove hover class
-                .off('touchend.fastanchor') // Unhook touchend
-                .off('touchcancel.fastanchor'); // Unhook touchcancel
-            $(window).off('scroll', scrollHandler);
+                .removeClass('active') // Remove active class
+                .off('.fastanchor'); // Unhook events
+                
+            $(window)
+                .off('scroll.fastanchor', scrollHandler) // Stop tracking scroll;
+        };
+        
+        $(window)
+            .on('scroll.fastanchor', scrollHandler); // Start tracking scroll to prevent "click" after scroll
+
+        var $this = $(event.target)
+                    .addClass('active') // Add active class
+         
+        $this.on('touchmove.fastanchor', function (event) { // Start tracking touch movement to see if we are still on top
+        
+            var touch = touchById(event.originalEvent.changedTouches, touchId);
+            if (!touch) return;
+
+            var element = document.elementFromPoint(touch.pageX, touch.pageY);
+            active = element && (this === element || $.contains(this, element));
+            $this.toggleClass('active', active);
             
-            if (!didScroll) {
+        }).on('touchend.fastanchor', function (event) {
+        
+            var touch = touchById(event.originalEvent.changedTouches, touchId);
+            if (!touch) return;
+            
+            onCancel();
+            
+            if (active && !didScroll) {
                 this.click(); // Trigger native event
+                event.preventDefault();
             }
-        }).on('touchcancel.fastanchor', function () {
-            $this
-                .removeClass('active') // Remove hover class
-                .off('touchend.fastanchor') // Unhook touchend
-                .off('touchcancel.fastanchor'); // Unhook touchcancel
-            $(window).off('scroll', scrollHandler);
-        });
+            
+        }).on('touchcancel.fastanchor', onCancel);
     
     });
 
